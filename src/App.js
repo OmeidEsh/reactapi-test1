aa4f63298c743827dc660fc6d7880e76da8f0396
Omeid Eshraghi <oeshraghi@kunaiconsulting.com>
2019-04-15 22:44:41 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users") //fetch the API from this URL
+      .then(res => res.json()) //Get the result "res" and convert it to json format
+      .then(json => {
+        this.setState({ isLoaded: true, items: json });
+      }); //Take the json that we just formated and create an arrow function to not lose contant.
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>Loading.....</div>;
+    } else {
+      return (
+        <div className="App">
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                Name:{item.name} ------ Email: {item.email} ------ Address:
+                {item.address.street}
+              </li>
+            ))}
+            ;
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
